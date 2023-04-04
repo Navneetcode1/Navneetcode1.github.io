6b50f5cd9fd63034fafc3438ec4de0bc5ec7e0f1
Navneet <navneetburman900@gmail.com>
2023-04-04 17:04:01 +0530

    added

--- navneetportfolio/src/Components/Calender.jsx
+++ navneetportfolio/src/Components/Calender.jsx
@@ -9,14 +9,14 @@ const Calender = () => {
       <Heading className='statHeading' as='h1' borderBottom='4px solid #a1a1a1' class="react-activity-calendar">Calendar & Stats</Heading>
       <Text className='statHeading' pb="25px" >Check out my Github Calendar & Streak Stats</Text>
       </Box>
-      <Box style={{textAlign:'center'}} className='calanderBox' data-aos="fade-up">
+      <Box style={{textAlign:'center'}} id="github-stats-card" className='calanderBox' data-aos="fade-up">
       <GitHubCalendar username="Navneetcode1" />
       </Box>
-      <Box className="statBox-dark" data-aos="fade-up">
+      <Box className="statBox-dark" data-aos="fade-up" id="github-stats-card">
         <Box className='statbox1-dark'>
         <img src="https://streak-stats.demolab.com/?user=Navneetcode1&theme=highcontrast&hide_border=true&border_radius=10" width='100%' alt="" />
         </Box>
-        <Box className='statbox1-dark' data-aos="fade-up">
+        <Box className='statbox1-dark' data-aos="fade-up" id="github-stats-card">
         <img src="https://github-stats-alpha.vercel.app/api?username=Navneetcode1&cc=000&tc=fff&ic=fff&bc=000" width='100%'/>
         </Box>
         </Box>
