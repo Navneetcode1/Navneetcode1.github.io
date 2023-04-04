e56e6c934acaea6a0f300a31cebcc16270798f59
Navneet <navneetburman900@gmail.com>
2023-04-04 15:50:05 +0530

    added

--- navneetportfolio/src/Components/Contact.jsx
+++ navneetportfolio/src/Components/Contact.jsx
@@ -16,6 +16,10 @@ const Contact = () => {
         </Box>
       <Text textAlign="center" color='white' m='0px' pb='5px'> Contact :- +91 8617555571</Text>
       <Text textAlign="center" color='white' m='0px' pb='5px'> Email :navneetburman900@gmail.com</Text>
+      <a href="https://github.com/Navneetcode1">
+          <Heading textDecoration="none" color="white" as="h4" className='Link_btns-dark' id="contact-github">Github</Heading></a>
+      <a href="https://www.linkedin.com/in/navneet-burman-b4b9181aa">
+          <Heading textDecoration="none" color="white" as="h4" className='Link_btns-dark' id="contact-linkedin">LinkedIn</Heading></a>
       <Text textAlign="center" color='white' m='0px' pb='5px'>Designed and build by Navneet Burman,<AiOutlineCopyright/> 2022 All rights reserved </Text>
     </Box>
   )
