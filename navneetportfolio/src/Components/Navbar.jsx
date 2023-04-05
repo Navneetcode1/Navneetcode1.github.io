a45b4e6e1e598af8651b39ae33805e8f9daee331
Navneet <navneetburman900@gmail.com>
2023-04-05 14:21:20 +0530

    added this file

--- navneetportfolio/src/Components/Navbar.jsx
+++ navneetportfolio/src/Components/Navbar.jsx
@@ -48,11 +48,11 @@ const scrollToHome = () => {
       <Box display='flex' id="nav-menu" >
         <p style={{fontSize:'40px',margin: '0px',marginLeft: '20px',fontFamily: 'cursive'}}>Navneet</p>
          <Flex gap={60}  margin='auto auto auto auto' >
-          <Heading as='h3'  className='Link_btns-dark' id="home" class="nav-link home" onClick={scrollToHome}>Home</Heading>
-          <Heading as='h3' className='Link_btns-dark' id="about" class="about-section" onClick={scrollToAbout}>AboutMe</Heading>
-          <Heading as='h3' className='Link_btns-dark' id="projects" class="nav-link projects" onClick={scrollToProjects}>Projects</Heading>
-          <Heading as='h3' className='Link_btns-dark' id="skills" class="nav-link skills" onClick={scrollToSkills}>Skills</Heading>
-          <Heading as='h3' className='Link_btns-dark' id="contact" class="nav-link contact" onClick={scrollToContact}>Contact</Heading>
+          <Heading as='h3'  className='Link_btns-dark' id="home" onClick={scrollToHome}>Home</Heading>
+          <Heading as='h3' className='nav-link about'  id="about"  class="nav-link about" onClick={scrollToAbout}>AboutMe</Heading>
+          <Heading as='h3' className='Link_btns-dark' id="projects" onClick={scrollToProjects}>Projects</Heading>
+          <Heading as='h3' className='Link_btns-dark'id="skills"  onClick={scrollToSkills}>Skills</Heading>
+          <Heading as='h3' className='Link_btns-dark' id="contact" onClick={scrollToContact}>Contact</Heading>
           <a id="resume-link-1" href="https://drive.google.com/file/d/1itS6azP58xHqeWGK68fR6s32DrJm352F/view?usp=sharing">
           <Heading textDecoration="none" color="white" as="h4" className='Link_btns-dark'  class="nav-link resume"  id="resume-button-1">Resume</Heading></a>
           </Flex>
